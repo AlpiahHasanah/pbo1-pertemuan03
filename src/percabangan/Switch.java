639163e3d2410e248964582a57fec0e937397d09
AlpiahHasanah <alpiahhasanah611@gmail.com>
2020-04-13 15:06:43 +0800

    percabangan switch halu

---
+++ src/percabangan/Switch.java
@@ -0,0 +1,24 @@
+package percabangan;
+
+public class Switch {
+    public static void main(String[] args) {
+        int jalanJalan=3;
+        switch (jalanJalan){
+            case '1':
+                System.out.println("uyang acayan");
+                break;
+            case '2':
+                System.out.println("uyang dubil dit");
+                break;
+            case '3':
+                System.out.println("uyang kawinan");
+                break;
+            case '4':
+                System.out.println("uyang beanakan");
+                break;
+            default:
+                System.out.println("HALU WKWKWKWKWKWK XDXD XOXO! @#$%%#");
+                break;
+        }
+    }
+}
